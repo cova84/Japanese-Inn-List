19fee75ab2c1f454a6e476f5618c7780c4fa59fe
小林由知 <kobayashiyoshitomo@USER-no-MacBook-Pro.local>
2017-11-16 02:50:39 +0800

    Page_layout_fix

---
+++ Japanese Inn List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Japanese Inn List
+//
+//  Created by 小林由知 on 2017/11/16.
+//  Copyright © 2017年 Yoshitomo Kobatashi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
